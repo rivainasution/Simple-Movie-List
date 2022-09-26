3f53cc954ac1b4551a150a62b606dfcf82b771d3
rivainasution <rivai_nasution@student.ub.ac.id>
2022-09-26 21:37:58 +0700

    Updates

---
+++ precache-manifest.a668b932b5e6d9205c9d29aaebfeb959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a824fa7040d15e98a7afce6e78359af",
+    "url": "/Simple-Movie-List/index.html"
+  },
+  {
+    "revision": "fca125a2474eb16c6f0d",
+    "url": "/Simple-Movie-List/static/css/2.0e7c9eac.chunk.css"
+  },
+  {
+    "revision": "eba7ef6f06ca1e2a3233",
+    "url": "/Simple-Movie-List/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "fca125a2474eb16c6f0d",
+    "url": "/Simple-Movie-List/static/js/2.9c6405f8.chunk.js"
+  },
+  {
+    "revision": "eba7ef6f06ca1e2a3233",
+    "url": "/Simple-Movie-List/static/js/main.73fb5e26.chunk.js"
+  },
+  {
+    "revision": "e8dc83d597fa6bb343c0",
+    "url": "/Simple-Movie-List/static/js/runtime-main.7d583cfc.js"
+  }
+]);
